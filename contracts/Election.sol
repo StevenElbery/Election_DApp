1d052644dd1b5cef46b9563bde28e1167e6a7d53
StevenElbery <stevenelbery@gmail.com>
2021-04-25 15:29:04 -0400

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.4.22;
+
+contract Election {
+    //Store candidate name
+    //Read candidate
+    string public candidate;
+    //Constructor
+    function Elections () public {
+        candidate = "Candidate 1";
+    }
+}
